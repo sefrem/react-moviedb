a911ce50b4f3f4268a2fcc864d0cfcfef687c89f
sefrem <47655937+sefrem@users.noreply.github.com>
2019-10-10 16:23:04 +0300

    Add files via upload

--- src/components/Movies/MovieItem.jsx
+++ src/components/Movies/MovieItem.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
+import { Star, Bookmark } from '@material-ui/icons';
+
+
+
 export default class MovieItem extends React.Component {
+  show = e => {
+    console.log(e.target)
+  }
   render() {
     const { item } = this.props;
     return (
@@ -15,6 +22,10 @@ export default class MovieItem extends React.Component {
           <h6 className="card-title">{item.title}</h6>
           <div className="card-text">Рейтинг: {item.vote_average}</div>
         </div>
+        <Star onClick={this.show}/>
+        <Bookmark onClick={this.show}/>
+        
+        
       </div>
     );
   }
