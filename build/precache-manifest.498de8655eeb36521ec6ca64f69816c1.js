9533046fc2ef886b938f701b321bd26727bd4730
sefrem <fireweb2112@gmail.com>
2020-01-09 12:25:46 +0400

    changed homepage

---
+++ build/precache-manifest.498de8655eeb36521ec6ca64f69816c1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd23fd642256360f3d8923d6d73208b5",
+    "url": "/sefrem/react-moviedb/index.html"
+  },
+  {
+    "revision": "b6dc642fa0b437b09f94",
+    "url": "/sefrem/react-moviedb/static/css/2.d52bfa24.chunk.css"
+  },
+  {
+    "revision": "4374b53474571f7c2068",
+    "url": "/sefrem/react-moviedb/static/css/main.905c5250.chunk.css"
+  },
+  {
+    "revision": "b6dc642fa0b437b09f94",
+    "url": "/sefrem/react-moviedb/static/js/2.6814761e.chunk.js"
+  },
+  {
+    "revision": "4374b53474571f7c2068",
+    "url": "/sefrem/react-moviedb/static/js/main.923180d5.chunk.js"
+  },
+  {
+    "revision": "1a7bfb4c67b7e667416e",
+    "url": "/sefrem/react-moviedb/static/js/runtime-main.e2a654e3.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/sefrem/react-moviedb/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/sefrem/react-moviedb/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/sefrem/react-moviedb/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/sefrem/react-moviedb/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  }
+]);
